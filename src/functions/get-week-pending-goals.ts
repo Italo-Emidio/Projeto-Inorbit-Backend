a23b5f989c9a1b9f9754f8cd976724fd20cae202
Italo-Emidio <italo.emidio7@gmail.com>
2024-09-12 13:40:28 -0300

    Foram adicionados os modelos das tabelas, rotas e as funcionalidades de criação e solicitação de metas

---
+++ src/functions/get-week-pending-goals.ts
@@ -0,0 +1,41 @@
+import dayjs from "dayjs";
+import weekOfYear from "dayjs/plugin/weekofYear";
+import { db } from "../db";
+import { goalCompletions, goals } from "../db/schema";
+import { and, count, gte, lte, sql } from "drizzle-orm";
+import { title } from "process";
+
+dayjs.extend(weekOfYear);
+
+export function getWeekPendingGoals() {
+  const firstDayOfWeek = dayjs().startOf("week").toDate();
+  const lastDayOfWeek = dayjs().endOf("week").toDate();
+
+  const goalsCreatedUpToWeek = db.$with("goals_created_up_to_week").as(
+    db
+      .select({
+        id: goals.id,
+        title: goals.title,
+        desiredWeeklyFrequency: goals.desiredWeeklyFrequency,
+        createdAt: goals.createdAt,
+      })
+      .from(goals)
+      .where(lte(goals.createdAt, lastDayOfWeek))
+  );
+
+  const goalCompletionCounts = db.$with("goal_completion_counts").as(
+    db
+      .select({
+        goalId: goalCompletions.goalId,
+        completionCount: count(goalCompletions.id),
+      })
+      .from(goalCompletions)
+      .where(
+        and(
+          gte(goals.createdAt, firstDayOfWeek),
+          lte(goals.createdAt, lastDayOfWeek)
+        )
+      )
+      .groupBy(goalCompletions.goalId)
+  );
+}
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,38 @@
 import fastify from "fastify";
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import { createGoal } from "../functions/create-goal";
+import z from "zod";
+import { resolve } from "path";
+import { title } from "process";
 
-const app = fastify();
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.post(
+  "/goals",
+  {
+    schema: {
+      body: z.object({
+        title: z.string(),
+        desiredWeeklyFrequency: z.number().int().min(1).max(7),
+      }),
+    },
+  },
+  async (request) => {
+    const { title, desiredWeeklyFrequency } = request.body;
+
+    await createGoal({
+      title,
+      desiredWeeklyFrequency,
+    });
+  }
+);
 
 app
   .listen({
