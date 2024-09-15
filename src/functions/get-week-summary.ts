7e96cfbad447579268156a7d41e73920cd45fbd9
Italo-Emidio <italo.emidio7@gmail.com>
2024-09-15 13:27:19 -0300

    Projeto Backend InOrbit Finalizado!

--- src/functions/get-week-summary.ts
+++ src/functions/get-week-summary.ts
@@ -1,4 +1,4 @@
-import { lte, count, and, gte, eq, sql } from "drizzle-orm";
+import { lte, count, and, gte, eq, sql, desc } from "drizzle-orm";
 import { db } from "../db";
 import { goalCompletions, goals } from "../db/schema";
 import dayjs from "dayjs";
@@ -53,7 +53,17 @@ export async function getWeekSummary() {
       })
       .from(goalsCompletedInWeek)
       .groupBy(goalsCompletedInWeek.completedAtDate)
+      .orderBy(desc(goalsCompletedInWeek.completedAtDate))
   );
+
+  type GoalsPerDay = Record<
+    string,
+    {
+      id: string;
+      title: string;
+      completedAt: string;
+    }[]
+  >;
 
   const result = await db
     .with(goalsCreatedUpToWeek, goalsCompletedInWeek, goalsCompletedByWeekDay)
@@ -65,7 +75,7 @@ export async function getWeekSummary() {
         sql`(select sum(${goalsCreatedUpToWeek.desiredWeeklyFrequency}) from ${goalsCreatedUpToWeek})`.mapWith(
           Number
         ),
-      goalsPerDay: sql`
+      goalsPerDay: sql<GoalsPerDay>`
         json_object_agg(
           ${goalsCompletedByWeekDay.completedAtDate},
           ${goalsCompletedByWeekDay.completions}
@@ -75,6 +85,6 @@ export async function getWeekSummary() {
     .from(goalsCompletedByWeekDay);
 
   return {
-    summary: result,
+    summary: result[0],
   };
 }
